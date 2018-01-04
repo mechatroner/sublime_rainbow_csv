93c133f4ba7f9d94d2820db472147b6f4ee11f09
mechatroner <mechatroner@yandex.ru>
2018-01-03 23:55:42 -0500

    programmatic color scheme change for filetype

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import sublime
 import sublime_plugin
 
+#class RainbowEventListener(sublime_plugin.EventListener):
+#    def on_load(self, view):
+#        view.run_command ("fold_by_level", { "level": 2 } )
 
-class CSVExampleCommand(sublime_plugin.TextCommand):
+
+class ExampleCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        #self.view.insert(edit, 0, "Hello, World!")
+        print("hello")
+        preferences = sublime.load_settings("csv.sublime-settings")
+        preferences.set("color_scheme", "Packages/Color Scheme - Default/Sixteen.tmTheme")
+        sublime.save_settings("csv.sublime-settings")
