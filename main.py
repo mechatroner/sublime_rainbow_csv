239bef8c148d3e0f457d8f9b66fcfc4d1a385cc9
mechatroner <mechatroner@yandex.ru>
2018-01-06 02:36:04 -0500

    improving

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import sublime
 import sublime_plugin
 
-#class RainbowEventListener(sublime_plugin.EventListener):
-#    def on_load(self, view):
-#        view.run_command ("fold_by_level", { "level": 2 } )
+def set_color_themes():
+    print("setting color theme")
+    preferences = sublime.load_settings("csv.sublime-settings")
+    preferences.set("color_scheme", "Packages/rainbow_csv/Rainbow.tmTheme")
+    sublime.save_settings("csv.sublime-settings")
+
+    preferences = sublime.load_settings("tsv.sublime-settings")
+    preferences.set("color_scheme", "Packages/rainbow_csv/Rainbow.tmTheme")
+    sublime.save_settings("tsv.sublime-settings")
 
 
-class ExampleCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        pass
-        #self.view.insert(edit, 0, "Hello, World!")
-        #print("hello")
+#class ExampleCommand(sublime_plugin.TextCommand):
+#    def run(self, edit):
+#        print("running example command")
+#        set_color_themes()
 
-        preferences = sublime.load_settings("csv.sublime-settings")
-        preferences.set("color_scheme", "Packages/rainbow_csv/Rainbow.tmTheme")
-        sublime.save_settings("csv.sublime-settings")
+
+class RainbowEventListener(sublime_plugin.EventListener):
+    def on_load(self, view):
+        #FIXME otimize: this works each time we open a new buffer
+        set_color_themes()
+
