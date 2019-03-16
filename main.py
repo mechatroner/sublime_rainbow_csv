1077372f3efacc5169b06e6f4c1558a75c43254a
mechatroner <mechatroner@yandex.ru>
2019-03-15 21:36:59 -0400

    cosmetic

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def get_syntax_before():
 
 def hex_to_rgb(hex_value):
     hex_value = hex_value.lstrip('#')
-    return tuple(int(hex_value[i:i+2], 16) for i in (0, 2 ,4))
+    return tuple(int(hex_value[i:i+2], 16) for i in (0, 2, 4))
 
 
 def do_adjust_color_scheme(style):
